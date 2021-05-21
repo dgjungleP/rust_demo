cc50b4bf8617166e10837c3d9d6a0efecb950cb4
jungle <dgjungle_c@163.com>
2021-05-22 01:56:46 +0800

    minigrep

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::fs;
+pub struct Config {
+    pub query: String,
+    pub file_name: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_name: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_name)?;
+    println!("With text:\n{}", contents);
+    Ok(())
+}
---
+++ minigrep/src/main.rs
@@ -0,0 +1,15 @@
+use minigrep::Config;
+use std::{env, process};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In the file {}", config.file_name);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    };
+}
