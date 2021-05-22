945d27db34ab3ef9ed173fb8f24cdc2edddcc19c
jungle <dgjungle_c@163.com>
2021-05-22 22:28:23 +0800

    env add

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,8 +1,9 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 pub struct Config {
     pub query: String,
     pub file_name: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -10,17 +11,24 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {
             query: args[1].clone(),
             file_name: args[2].clone(),
+            case_sensitive,
         })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_name)?;
-    for line in search(&config.query, &contents) {
+
+    let result = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in result {
         println!("{}", line);
     }
 
@@ -37,17 +45,41 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     result
 }
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn on_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
